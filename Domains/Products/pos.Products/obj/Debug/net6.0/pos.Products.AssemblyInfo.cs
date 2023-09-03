92c0bf22a2f2324701a9023736614903240445ee
justaman2712 <letuanbao@mbg8.onmicrosoft.com>
2023-09-03 09:48:26 +0700

    apply migration

--- Domains/Products/pos.Products/obj/Debug/net6.0/pos.Products.AssemblyInfo.cs
+++ Domains/Products/pos.Products/obj/Debug/net6.0/pos.Products.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos.Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa568d840de5b2ffc5f97327439bdd4b658473d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos.Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos.Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
